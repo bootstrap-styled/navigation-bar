9f00d7eefe9763644c5e3f764e1f33ad92a79557
alexander janet <ajt@nelson-02.nelson.yeutech.vn>
2018-10-10 11:15:48 +1300

    fix(styleguide/setup): modified bootstrap-styled/v4 imports

--- styleguide/setup.js
+++ styleguide/setup.js
@@ -1,9 +1,9 @@
 /* Ignored components needed to be used in example */
-import ListGroup from 'bootstrap-styled/lib/ListGroup';
-import Button from 'bootstrap-styled/lib/Button';
-import Ul from 'bootstrap-styled/lib/Ul';
-import Li from 'bootstrap-styled/lib/Li';
-import A from 'bootstrap-styled/lib/A';
+import ListGroup from '@bootstrap-styled/v4/lib/ListGroup';
+import Button from '@bootstrap-styled/v4/lib/Button';
+import Ul from '@bootstrap-styled/v4/lib/Ul';
+import Li from '@bootstrap-styled/v4/lib/Li';
+import A from '@bootstrap-styled/v4/lib/A';
 
 global.ListGroup = ListGroup;
 global.Button = Button;
